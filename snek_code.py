12befb0a78baaac8a12e9328263a064c311d93fb
Justin Yi <jdyimail@sbcglobal.net>
2017-09-15 14:31:36 -0700

    added a screen

--- snek_code.py
+++ snek_code.py
@@ -1,4 +1,11 @@
 import pygame
+import random
 
-#tryin' to make a change :/
-#new
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+
+size = [800, 800] #Should always be a square and multiple of 4
+screen = pygame.display.set_mode(size)
